2508211aadba3eb2110ae4d50cf82b118119cad3
nikkehtine <nikkehtine@int.pl>
2023-05-21 18:27:22 +0200

    Make build script actually work

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,9 @@
 import * as fs from "node:fs";
 
+const path = "./projects";
+
 const getDirs = () => {
-  const path = "./pages/";
   return fs.readdirSync(path).filter((file) => fs.statSync(path + "/" + file).isDirectory());
 };
 
-console.log(getDirs());
+process.env.projects = getDirs();
