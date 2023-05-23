6a4e29906f57766e2cf0d52e99e3303be0a3c91a
nikkehtine <nikkehtine@int.pl>
2023-05-23 15:32:55 +0200

    Add router

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import http from "http";
+
+export default function serve(list) {
+  const server = http.createServer();
+  const url = request.url;
+
+  server.on("request", (request, response) => {
+    switch (url) {
+      default:
+        // 404
+        break;
+    }
+  });
+}
+
+function routes(list) {}
