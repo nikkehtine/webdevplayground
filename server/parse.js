2a6c4fe0f92dd83d56725339066cc00b4f83394b
nikkehtine <nikkehtine@int.pl>
2023-05-23 14:26:18 +0200

    Use routing in parse.js

--- server/parse.js
+++ server/parse.js
@@ -13,7 +13,7 @@ function addEntry(obj) {
   const description = `<p class="description">${obj.description}</p>`;
 
   function linkify(element) {
-    return `<a href="${obj.entry}">${element}</a>`;
+    return `<a href="/${obj.name}">${element}</a>`;
   }
 
   return `<div class="project">\n  ${linkify(title)}\n  ${linkify(description)}\n</div>`;
